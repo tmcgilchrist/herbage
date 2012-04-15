a7247a031db8515a7e31efe585c86b0f1d061e6e
Tim McGilchrist <timmcgil@gmail.com>
2012-04-15 10:32:08 +1000

    All generated test working now.

--- spec/views/plants/index.html.erb_spec.rb
+++ spec/views/plants/index.html.erb_spec.rb
@@ -4,24 +4,26 @@ describe "plants/index" do
   before(:each) do
     assign(:plants, [
       stub_model(Plant,
-        :name => "Name",
+        :name => "Plant Name",
         :common_names => "Common Names",
+        :species => "Species",
         :genus => "Genus",
-        :description => "MyText",
-        :care => "MyText",
-        :soil => "MyText",
-        :position => "MyText",
-        :colour => "Colour"
+        :description => "Pretty yellow plant",
+        :care => "Water well",
+        :soil => "Acidic well mulched",
+        :position => "Sunny",
+        :colour => "Yellow"
       ),
       stub_model(Plant,
-        :name => "Name",
+        :name => "Plant Name",
         :common_names => "Common Names",
+        :species => "Species",
         :genus => "Genus",
-        :description => "MyText",
-        :care => "MyText",
-        :soil => "MyText",
-        :position => "MyText",
-        :colour => "Colour"
+        :description => "Pretty yellow plant",
+        :care => "Water well",
+        :soil => "Acidic well mulched",
+        :position => "Sunny",
+        :colour => "Yellow"
       )
     ])
   end
@@ -29,13 +31,14 @@ describe "plants/index" do
   it "renders a list of plants" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => "Plant Name".to_s, :count => 2
     assert_select "tr>td", :text => "Common Names".to_s, :count => 2
+    assert_select "tr>td", :text => "Species".to_s, :count => 2
     assert_select "tr>td", :text => "Genus".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => "Colour".to_s, :count => 2
+    assert_select "tr>td", :text => "Pretty yellow plant".to_s, :count => 2
+    assert_select "tr>td", :text => "Water well".to_s, :count => 2
+    assert_select "tr>td", :text => "Acidic well mulched".to_s, :count => 2
+    assert_select "tr>td", :text => "Sunny".to_s, :count => 2
+    assert_select "tr>td", :text => "Yellow".to_s, :count => 2
   end
 end

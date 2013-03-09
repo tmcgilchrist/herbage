ac633952bcf389ed538fa850fcb082a7e3a3bd4e
Tim McGilchrist <timmcgil@gmail.com>
2013-03-10 10:52:08 +1100

    Basic homepage with information.
    and by basic I mean ugly.

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,7 @@ Herbage::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'plants#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
